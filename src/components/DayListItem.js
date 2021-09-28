dd5c074ff4e574ccba3ef12a77cd1b8623d013c2
zak <zakwarsamee@gmail.com>
2021-09-28 17:04:37 -0400

    implement daylistitem and ensure it passes the tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classnames from "classnames";
 
 export default function DayListItem(props) {
+  let itemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots,
+  });
+
+  const formatSpots = (spots) => {
+    if (!spots) return "no spots remaining";
+    return (spots += spots === 1 ? " spot remaining" : " spots remaining");
+  };
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)} className={itemClass}>
+      <h2>{props.name}</h2>
+      <h3> {formatSpots(props.spots)}</h3>
     </li>
   );
 }
