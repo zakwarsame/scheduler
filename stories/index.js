ea58c5f7ce2fa7e0f7d5931749d17f98ca8618a5
zak <zakwarsamee@gmail.com>
2021-09-28 21:53:15 -0400

    confirmed daylist and daylist items are working using storybook

--- stories/index.js
+++ stories/index.js
@@ -6,7 +6,8 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
-import DayListItem from 'components/DayListItem'
+import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 
 storiesOf("Button", module)
   .addParameters({
@@ -35,5 +36,31 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
-  
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
