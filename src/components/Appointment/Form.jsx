79ca932134d4c79019f8da15aed2fadcf7f8edc1
zak <zakwarsamee@gmail.com>
2021-09-29 23:19:06 -0400

    update the appointment with empty, error, form, header, show and status components

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+    const [name, setName] = useState(props.name || "");
+    const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+    // reset function
+    const reset = () => {
+        setName("");
+        setInterviewer(null)
+    }
+
+    // cancel function
+    const cancel = () => {
+        reset()
+        props.onCancel();
+    }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            value={name}
+            placeholder="Enter Student Name"
+            onChange={event => setName(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button onClick={props.onSave} confirm>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      { props.interview ? <Show /> : <Empty/> }
+    </article>
+  );
+}
