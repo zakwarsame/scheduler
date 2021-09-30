b8931a90088dc2b316608921cda2345335ea5a8b
zak <zakwarsamee@gmail.com>
2021-09-29 23:25:54 -0400

    add mock data to fully test appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,6 +21,25 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
