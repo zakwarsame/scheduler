55a767a9a78aec56c3acc3d9b2501bc40126f593
zak <zakwarsamee@gmail.com>
2021-10-06 18:29:34 -0400

    install cypress and test navigation - will add data-testid to DayListItem to test element

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,11 +11,12 @@ export default function DayListItem(props) {
 
   const formatSpots = (someSpots) => {
     if (!someSpots) return "no spots remaining";
-    return (someSpots += someSpots === 1 ? " spot remaining" : " spots remaining");
+    return (someSpots +=
+      someSpots === 1 ? " spot remaining" : " spots remaining");
   };
 
   return (
-    <li onClick={() => setDay(name)} className={itemClass}>
+    <li onClick={() => setDay(name)} data-testid={"day"} className={itemClass}>
       <h2>{name}</h2>
       <h3> {formatSpots(spots)}</h3>
     </li>
