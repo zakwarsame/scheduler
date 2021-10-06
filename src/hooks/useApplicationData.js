51b0c64e7e61b2be99488e7c42100a17065b7f37
zak <zakwarsamee@gmail.com>
2021-10-06 01:46:21 -0400

    implement reducer using an object instead of switch and commented it out

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,65 +1,123 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import axios from "axios";
 
 export default function useApplicationData(initial) {
-  const [state, setState] = useState({
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+
+  const reducers = {
+    SET_DAY(state, action) {
+      return { ...state, day: action.day };
+    },
+    SET_INTERVIEW(state, action) {
+      const updateSpots = (appointments) => {
+        const currentDay = state.days.filter((day) => day.name === state.day);
+        let counter = 0;
+        for (let key of currentDay[0].appointments) {
+          if (!appointments[key].interview) {
+            counter++;
+          }
+        }
+        const newDay = { ...currentDay[0], spots: counter };
+        const days = state.days.map((day) => {
+          return day.id === newDay.id ? newDay : day;
+        });
+
+        return days;
+      };
+      const appointment = {
+        ...state.appointments[action.id],
+        interview: action.interview && { ...action.interview },
+      };
+      const appointments = {
+        ...state.appointments,
+        [action.id]: appointment,
+      };
+      const days = updateSpots(appointments);
+      return { ...state, appointments, days };
+    },
+
+    SET_APPLICATION_DATA(state, action) {
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers,
+      };
+    },
+    default: action => new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`)
+  };
+
+  function reducer(state, action) {
+    return (reducers[action.type](state, action)|| reducers.default )
+  }
+
+  // function reducer(state, action) {
+  //   switch (action.type) {
+  //     case SET_DAY:
+  //       return { ...state, day: action.day };
+  //     case SET_APPLICATION_DATA:
+  //       return {
+  //         ...state,
+  //         days: action.days,
+  //         appointments: action.appointments,
+  //         interviewers: action.interviewers,
+  //       };
+  //     case SET_INTERVIEW: {
+  //       const updateSpots = (appointments) => {
+  //         const currentDay = state.days.filter((day) => day.name === state.day);
+  //         let counter = 0;
+  //         for (let key of currentDay[0].appointments) {
+  //           if (!appointments[key].interview) {
+  //             counter++;
+  //           }
+  //         }
+  //         const newDay = { ...currentDay[0], spots: counter };
+  //         const days = state.days.map((day) => {
+  //           return day.id === newDay.id ? newDay : day;
+  //         });
+
+  //         return days;
+  //       };
+  //       const appointment = {
+  //         ...state.appointments[action.id],
+  //         interview: action.interview && { ...action.interview },
+  //       };
+  //       const appointments = {
+  //         ...state.appointments,
+  //         [action.id]: appointment,
+  //       };
+  //       const days = updateSpots(appointments);
+  //       return { ...state, appointments, days };
+  //     }
+  //     default:
+  //       throw new Error(
+  //         `Tried to reduce with unsupported action type: ${action.type}`
+  //       );
+  //   }
+  // }
+
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   });
-  const setDay = (day) => setState({ ...state, day });
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
 
   const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    const days = updateSpots(appointments);
-
     return axios.put(`/api/appointments/${id}`, { interview }).then((res) => {
-      setState({ ...state, appointments, days });
+      dispatch({ type: SET_INTERVIEW, id, interview });
     });
   };
 
   const cancelInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-    const days = updateSpots(appointments);
-
     return axios
       .delete(`/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments, days }));
+      .then(() => dispatch({ type: SET_INTERVIEW, id, interview: null }));
   };
-
-  const updateSpots = (appointments) => {
-    const currentDay = state.days.filter((day) => day.name === state.day);
-    let counter = 0;
-    for (let key of currentDay[0].appointments) {
-      if (!appointments[key].interview) {
-        counter++;
-      }
-    }
-    const newDay = { ...currentDay[0], spots: counter };
-    const days = state.days.map((day) => {
-      return day.id === newDay.id ? newDay : day;
-    });
-
-    return days;
-  };
-
-  //   updateSpots(state)
 
   useEffect(() => {
     const fetchDays = axios.get("/api/days");
@@ -68,12 +126,20 @@ export default function useApplicationData(initial) {
 
     Promise.all([fetchDays, fetchAppoitments, fetchInterviewers]).then(
       (all) => {
-        setState((prev) => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        }));
+        dispatch(
+          {
+            type: SET_APPLICATION_DATA,
+            days: all[0].data,
+            appointments: all[1].data,
+            interviewers: all[2].data,
+          }
+          //   (prev) => ({
+          //   ...prev,
+          //   days: all[0].data,
+          //   appointments: all[1].data,
+          //   interviewers: all[2].data,
+          // }
+        );
       }
     );
   }, []);
